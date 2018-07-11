d3ab1b560950a28b694e481220915efda6db583d
bloodstrawberry <40860674+bloodstrawberry@users.noreply.github.com>
2018-07-11 14:09:35 +0900

    Create SelectionSort.c

---
+++ Sorting/BubbleSort/SelectionSort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void SelSort(int arr[], int n)
+{
+	int i, j;
+	int maxIdx;
+	int tmp;
+
+	for (i = 0; i < n - 1;i++)
+	{
+		maxIdx = i;
+
+		for (j = i + 1;j < n;j++)
+			if (arr[j] < arr[maxIdx]) maxIdx = j;
+		
+		tmp = arr[i];
+		arr[i] = arr[maxIdx];
+		arr[maxIdx] = tmp;
+	}
+}
+
+int main()
+{
+	int arr[4] = { 3,2,4,1 };
+	int i;
+
+	SelSort(arr, sizeof(arr) / sizeof(int));
+	
+	for (i = 0; i < 4;i++)
+		printf("%d ", arr[i]);
+
+	putchar('\n');
+
+	return 0;
+}
